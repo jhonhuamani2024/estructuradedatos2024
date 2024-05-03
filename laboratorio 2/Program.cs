7483826c033de7b86eab022bad3baab143542713
jhonhuamani2024 <168664371+jhonhuamani2024@users.noreply.github.com>
2024-05-03 17:30:20 -0500

    cambiiiii

---
+++ laboratorio 2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Suma de n primeros numeros 
+var n = 4;
+var suma = 0;
+for (var i = 1; i <= n; i++)
+{
+    suma += i;
+}
+Console.WriteLine($"la suma de los {n} numeros naturales son:{suma}");
