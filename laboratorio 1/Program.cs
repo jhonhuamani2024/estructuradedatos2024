328a839bbc312bc19fbb4c44f7af264f6415680c
jhonhuamani2024 <168664371+jhonhuamani2024@users.noreply.github.com>
2024-05-14 10:16:50 -0500

    arreglado

--- laboratorio 1/Program.cs
+++ laboratorio 1/Program.cs
@@ -1,30 +1,8 @@
-﻿using System;
-
-class Program
+﻿// suma de n primeros numeros 
+var n = 8;
+var suma = 0;
+for (var i = 1; i <= n; i++)
 {
-    static void Main()
-    {
-        Console.WriteLine("Ingrese el tamaño del arreglo:");
-        int tamano = Convert.ToInt32(Console.ReadLine());
-
-        int[] arreglo = new int[tamano];
-
-        // Solicitar al usuario que ingrese los elementos del arreglo
-        for (int i = 0; i < tamano; i++)
-        {
-            Console.WriteLine($"Ingrese el elemento {i + 1}:");
-            arreglo[i] = Convert.ToInt32(Console.ReadLine());
-        }
-
-        // Calcular la suma de los elementos del arreglo
-        int suma = 0;
-        foreach (int num in arreglo)
-        {
-            suma += num;
-        }
-
-        // Mostrar la cantidad de elementos y la suma
-        Console.WriteLine($"La cantidad de elementos del arreglo es: {tamano}");
-        Console.WriteLine($"La suma de los elementos del arreglo es: {suma}");
-    }
+    suma += i;
 }
+Console.WriteLine($"la suma de los {n} numeros naturales son:{suma}");
---
+++ laboratorio 4 y 5/funcion recursiva.cs
@@ -0,0 +1,95 @@
+﻿
+//Recursividad:
+//1) Ejercicio 1: Escribe una función recursiva que imprima los números pares del 1 al 100.
+//class Pares
+//{
+//    static void Main(string[] args)
+//    {
+//        PrintEvenNumbers(1);
+//    }
+//    static void PrintEvenNumbers(int n)
+//    {
+//        if (n <= 100)
+//        {
+//            if (n % 2 == 0)
+//            {
+//                Console.WriteLine(n);
+//            }
+//            PrintEvenNumbers(n + 1);
+//        }
+//    }
+//}
+
+//2) Ejercicio 2: Escribe una función recursiva que imprima la suma de los números del 1 al n.
+//class suma
+//{
+//    static void Main(string[] args)
+//    {
+//        int n = 10; //podemos cambiar con cualquier numero que necesitemos  
+//        Console.WriteLine($"La suma de los números del 1 al {n} es: {SumNumeros(1, n)}");
+//    }
+
+//    static int SumNumeros(int start, int end)
+//    {
+//        if (start == end)
+//        {
+//            return start;
+//        }
+//        else
+//        {
+//            return start + SumNumeros(start + 1, end);
+//        }
+//    }
+//}
+
+
+//3) Ejercicio 3: Escribe una función recursiva que imprima la pirámide de números del 1 al n.
+//class priramide
+//{
+//    static void Main(string[] args)
+//    {
+//        int n = 100; // Puedes cambiar el valor de n según tus necesidades
+//        Console.WriteLine($"La suma de los números del 1 al {n} es: {SumNumbers(1, n)}");
+//    }
+
+//    static int SumNumbers(int start, int end)
+//    {
+//        if (start == end)
+//        {
+//            return start;
+//        }
+//        else
+//        {
+//            return start + SumNumbers(start + 1, end);
+//        }
+//    }
+//}
+
+//4) Ejercicio 4: Escribe una función recursiva que imprima la pirámide de números invertidos del 1 al n.
+//class invertido
+//{
+//    static void Main(string[] args)
+//    {
+//        int n = 5; // Puedes cambiar el valor de n según tus necesidades
+//        PrintInvertedPyramid(n, n);
+//    }
+
+//    static void PrintInvertedPyramid(int current, int n)
+//    {
+//        if (current == 0)
+//            return;
+
+//        PrintRow(n - current + 1, current);
+//        PrintInvertedPyramid(current - 1, n);
+//    }
+
+//    static void PrintRow(int start, int end)
+//    {
+//        if (start > end)
+//            return;
+
+//        Console.Write($"{end - start + 1} ");
+//        PrintRow(start + 1, end);
+//    }
+//}
+
